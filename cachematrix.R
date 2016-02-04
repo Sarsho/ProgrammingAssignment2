5e0559de4da2ec680a8495610757c1762b316dbe
Austin Overman <aoverman@cox.net>
2016-02-03 22:08:04 -0700

    Near complete re-write to include a modified stub code
    Using the stub code and example provided, the code was modified to
    create and then solve for the inverse of a square (invertible) matrix,
    store the inverse and then check for solution in subsequent calls for
    the inverse instead of re-solving the inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes a list of otherwise defined matrix (invertible) and saves it 
+## to someplace I need to better understand. I know that the assignment operator
+## <<- saves and updates to the symbol/variable in the parent environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinvrs <- function(invrs) m <<- invrs
+      getinvrs <- function() m
+      list(set = set, get = get,
+            setinvrs = setinvrs,
+            getinvrs = getinvrs)
 }
 
 
-## Write a short comment describing this function
+## This function is detemining if the matrix inverse has been calculated by assuming
+## that if 'm' is not null and the matrix has not changed that the inverse had been
+## calculated and returns the inverse. If 'm' is null then the inverse is calculated
+## stored for further use.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinvrs()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinvrs(m)
+      m
 }
